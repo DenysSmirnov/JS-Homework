c8e99413d744a3c380a58e0e481592eb88770b06
DenysSmirnov <densmith77@gmail.com>
2018-03-06 16:43:46 +0200

    added version for small devices (640dpi)

--- homework04/js/script.js
+++ homework04/js/script.js
@@ -1,123 +1,103 @@
+(function($){
+function l(...args){
+	console.log(...args);
+}
+
 $('img').hover( function(e) {
     $(e.target).closest('.sale-photo').find('.shop-it').css("display", "block");
 }, function(e) {
     $(e.target).closest('.sale-photo').find('.shop-it').css("display", "");
 })
 
-// $('ul.tabset').on('click', function(e) {
-// 	e.preventDefault();
-// 	// console.log($(e.target).text());
-// 	// let text = $(e.target).text().replace(' ', '-').replace("'", '').replace('?', '');
-// 	let text = $(e.target).text().replace(/[\s'?]/g, '');
-// 		$('ul.tabset li').removeClass('active');
-// 		$(e.target).parent().addClass('active');
-// 		$('div.tab.active').removeClass('active');
-// 		$('#tab-' + text).addClass('active');
-
-// 	// slides = $(".tab.active .article");
-// 	// width = $(".tab.active .article").width()+13;
-// 	// i = slides.length;
-// 	// offset = 0;
-
-// })
-$(window).on('resize', carousel);
+$(window).on('resize', resize);
 carousel();
+carousel2();
+
+function resize() {
+	var width = 1200;
+	if ($(document).width() < $(".gallery").width()+1) {
+		width = $(document).width();
+	}
+	$(".gallery ul li").css('width', $("div.gallery").width()+'px');
+	return width;
+}
 
 function carousel(){
 	var slides = $(".gallery ul li");
-	if ($(document).width() < $(".gallery").width()+1) {
-		var width = $(document).width();
-	} else {
-		var width = 1200;
-	}
-	slides.css('width', $("div.gallery").width()+'px');
-	var i = slides.length-1;
+	width = resize();
+	var i = 1;
 	var offset = 0;
+l('№'+i, 'off '+offset, width);
 	$('.gallery a.next').on('click', function(e) {
 		e.preventDefault();
-	console.log(i, offset, width);/////////////////////
-		if (i > 0) {
+		if (i < slides.length) {
 			offset += width;
 		} else {
-			i = slides.length;
+			i = 0;
 			offset = 0;
 		}
 		$('.gallery ul').css({
 			"-webkit-transform":"translateX(-"+offset+"px)",
 			"-webkit-transition":"all 1s ease-in-out"
 		});
-		i--;
+		i++; l('№'+i, 'off '+offset, width);
 	})
+
+
 	$('.gallery a.prev').on('click', function(e) {
 		e.preventDefault();
-	console.log(i, offset, width);/////////////////////
 		if (offset > 0) {
 			offset -= width;
 		} else {
-			offset = width * i;
-			i = 0;
+			offset = width * (slides.length - 1);
+			i = slides.length+1;
 		}
 		$('.gallery ul').css({
 			"-webkit-transform":"translateX(-"+offset+"px)",
 			"-webkit-transition":"all 1s ease-in-out"
 		});
-		i++;
+		i--; l('№'+i, 'off '+offset, width);
 	})
 }
 
-$(document).ready(function xxx() { // Ждём загрузки страницы
-						   
-	var slides = $(".tab.active .article"); // Получаем массив всех слайдов
-	var width = $(".tab.active .article").width()+13; // Получаем ширину 1 артикла
-	// var width = $(document).width();
-	var i = slides.length; // Получаем количество слайдов
-	var offset = i * width; // Задаем начальное смещение и ширину всех слайдов
-	// var offset = $(document).width();
-	i--; // уменьшаем кол-во слайдов на один ( для проверки в будущем )
-	// i = i-4;
-// console.log(slides, i, offset);
-	// $(".tab.active").css('width',offset); // Задаем блоку со слайдами ширину всех слайдов
-	
-	offset = 0; // Обнуляем смещение, так как показывается cначала 1 слайд
-	$(".btn-tabs-next").on('click', function(){	// Событие клика на кнопку "следующий слайд"
-		console.log(i, offset, width * i+width);
-		if (offset < width * i) {	// Проверяем, дошли ли мы до конца
-			offset += width; // Увеличиваем смещение до следующего слайда
-			$(".tab.active").css({ 	// Смещаем блок со слайдами к следующему
+function carousel2() {				   
+	var slides = $(".tab.active .article");
+	var width = $(".tab.active .article").width()+13;
+	var i = slides.length;
+	var offset = 0;
+	var xxx = $(document).width();
+	$(".btn-tabs-next").on('click', function(){
+		if (offset < width * i - xxx) {
+			offset += width;
+			$(".tab.active").css({
 				"-webkit-transform":"translateX(-"+offset+"px)",
 				"-webkit-transition":"all 1s ease-in-out"
-				});
-			i = i-2;
+			});
+			i--;
 		}
 	});
 
-	$(".btn-tabs-prev").on('click', function(){	// Событие клика на кнопку "предыдущий слайд"
-		console.log(offset, width*i-width);
-		if (offset > 0) { // Проверяем, дошли ли мы до конца
-			offset -= width; // Уменьшаем смещение до предыдущего слайда
-			$(".tab.active").css("-webkit-transform","translateX(-"+offset+
-			"px)").css("-webkit-transition","all 1s ease-in-out"); // Смещаем блок со слайдами к предыдущему
-			i = i+2;
+	$(".btn-tabs-prev").on('click', function(){
+		if (offset > 0) {
+			offset -= width;
+			$(".tab.active").css({
+				"-webkit-transform":"translateX(-"+offset+"px)",
+				"-webkit-transition":"all 1s ease-in-out"
+			});
+			i++;
 		}
 	});
+}
 
-	$('ul.tabset').on('click', function(e) {
-		e.preventDefault();
-		// console.log($(e.target).text());
-		// let text = $(e.target).text().replace(' ', '-').replace("'", '').replace('?', '');
-		let text = $(e.target).text().replace(/[\s'?]/g, '');
-			$('ul.tabset li').removeClass('active');
-			$(e.target).parent().addClass('active');
-			$('div.tab.active').removeClass('active');
-			$('#tab-' + text).addClass('active');
+$('ul.tabset li').on('click', function(e) {
+	e.preventDefault();
+	let text = $(e.target).text().replace(/[\s'?]/g, '');
+	$('ul.tabset li.active').removeClass('active').show();
+	$(e.target).parent().addClass('active').siblings().toggle();
+	$('div.tab.active').removeClass('active');
+	$('#tab-' + text).addClass('active');
 
-		// slides = $(".tab.active .article");
-		// width = $(".tab.active .article").width()+13;
-		// i = slides.length;
-		// offset = 0;
-		xxx();
-		console.log(i)
+	carousel2();
+})
 
-	})
-
-});
+})(jQuery);
--- homework04/js/sidebar.js
+++ homework04/js/sidebar.js
@@ -1,10 +1,19 @@
 $('.burger').on('click', function(e) {
+    let parent = $(e.target).parent();
+    console.log(parent);
     e.preventDefault();
     $('.sidebar-wrapper').css('display', 'block');
-    $('.sidebar').addClass('-up');
+    if ( parent.hasClass('menu') ) {
+        $('.sidebar').addClass('-up');
+    } else {
+        $('.footer-nav').addClass('up');
+        $('.footer-nav button').css('display', 'block');
+    }
 })
 
 $('.sidebar-wrapper, .btn-close').mouseup(function() {
     $('.sidebar').removeClass('-up');
+    $('.footer-nav').removeClass('up');
     $('.sidebar-wrapper').css('display', 'none');
+    $('.footer-nav button').css('display', 'none');
 })
